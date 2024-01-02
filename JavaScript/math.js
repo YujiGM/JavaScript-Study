059f8740ba36d94ab4d2d23f9e67eaca9ae7e839
YujiGM <125309933+YujiGM@users.noreply.github.com>
2024-01-02 15:26:54 -0300

    math.js

---
+++ JavaScript/math.js
@@ -0,0 +1,86 @@
+//This study will show some of JavaScript's Math.
+
+//Round will return the rounded value of a, which is 3.
+let a = 3.14;
+
+x = Math.round(a);
+
+console.log(a);
+
+------------------------
+//Floor will return the round down value of b, which is 3.
+let b = 3.99;
+
+b = Math.floor(b);
+
+console.log(b);
+
+------------------------
+//Ceil will return the value of round up c, which is 4.
+let c = 3.14;
+
+c = Math.ceil(c);
+
+console.log(c);
+
+------------------------
+//Pow will return the power of d, which is 9.
+
+let d = 3;
+  
+d = Math.pow(d, 2);
+
+console.log(d);
+
+------------------------
+//Sqrt will return the square root of e, which is 2.
+
+let e = 4;
+
+e = Math.sqrt(e);
+
+console.log(e);
+
+------------------------
+//Abs return tha distance from 0, which is positive 5.
+
+let f = -5
+
+f = Math.abs(f)
+
+console.log(f)
+
+------------------------
+//Max will show the maximus number, which is 4.
+
+let x = 2;
+let y = 3.2;
+let z = 4;
+
+let maximum;
+
+maximum = Math.max(x, y, z);
+
+console.log(maximum);
+
+------------------------
+//Min will show the minimum number, which is 2.
+
+let x = 2;
+let y = 3.2;
+let z = 4;
+
+let minimum;
+
+minimum = Math.min(x, y, z);
+
+console.log(minimum);
+
+
+
+
+
+
+
+
+
