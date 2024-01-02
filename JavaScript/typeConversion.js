7a5d0a8aefc810d6b38fd7fed9baf1f10037d8a5
YujiGM <125309933+YujiGM@users.noreply.github.com>
2024-01-02 14:40:22 -0300

    Create typeConversion.js

---
+++ JavaScript/typeConversion.js
@@ -0,0 +1,23 @@
+//To convert any string to a number, use:
+
+let x;
+
+let x = Number("10");
+
+//To convert a number into a string, use:
+
+let y;
+
+let y = String(32);
+
+//If you use an empty string in boolean constructor, it will return False.
+
+let z;
+
+let z = Boolean("");
+
+//But, if the string isn't empty, it will return True.
+
+let z;
+
+let z = Boolean("hi");
